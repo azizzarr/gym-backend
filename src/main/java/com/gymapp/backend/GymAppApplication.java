0b9d4cd9f29b2db8123fe7f04b40250780655c22
azizzarr <75136140+azizzarr@users.noreply.github.com>
2025-04-06 20:41:34 +0100

    Update application.properties with fixed security configuration

--- src/main/java/com/gymapp/backend/GymAppApplication.java
+++ src/main/java/com/gymapp/backend/GymAppApplication.java
@@ -2,15 +2,8 @@ package com.gymapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    ReactiveUserDetailsServiceAutoConfiguration.class,
-    ReactiveSecurityAutoConfiguration.class,
-    UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication
 public class GymAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymAppApplication.class, args);
