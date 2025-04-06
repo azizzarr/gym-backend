78ba60fd0cfe97df73a4734fe709c3c22febf7fc
azizzarr <75136140+azizzarr@users.noreply.github.com>
2025-04-06 06:30:08 +0100

    Update backend configuration and dependencies

--- src/main/java/com/gymapp/backend/GymAppApplication.java
+++ src/main/java/com/gymapp/backend/GymAppApplication.java
@@ -2,8 +2,15 @@ package com.gymapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    ReactiveUserDetailsServiceAutoConfiguration.class,
+    ReactiveSecurityAutoConfiguration.class,
+    UserDetailsServiceAutoConfiguration.class
+})
 public class GymAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymAppApplication.class, args);
